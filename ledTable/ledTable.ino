b50060170fbfca959b32aa3dac27a2d8ed1a099d
Robin Kock <robin@kock-hamburg.de>
2017-11-03 15:14:09 +0100

    further testing

--- ledTable/ledTable.ino
+++ ledTable/ledTable.ino
@@ -19,7 +19,7 @@ uint8_t h = 0;
 void loop()
 {
     for (uint16_t i = 0; i < LEDS_NUM; i++)
-        leds[i].setRGB((i * 11) % 255, h, (i * h) % 255);
+        leds[i].setHSV((i + h) % 256, 255, 100);
     FastLED.show();
 
     h++;
