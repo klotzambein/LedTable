f0c4b5330ca55ad06ef8a90d77a67d9cd6a900ec
Robin Kock <robin@kock-hamburg.de>
2017-11-19 12:30:14 +0100

    New Text

--- ledTable/ledTable.ino
+++ ledTable/ledTable.ino
@@ -8,19 +8,19 @@ void setup()
 }
 
 //                      m   a  v   e  r   i  c  k   STOP
-const uint8_t text[] = {12, 0, 21, 4, 17, 8, 2, 10, 0xFF};
+const uint8_t text[] = {13, 8, 2, 14, 36, 8, 17, 18, 19, 36, 6, 4, 8, 11, 18, 19, 4, 0xFF};
 
 uint8_t h = 0;
 uint8_t t = 0;
 void loop()
 {
     for (uint16_t i = 0; i < LEDS_NUM; i++)
-        setHSV_Z(i, (i + h) % 256, 255, 30);
+        setHSV_Z(i, (i + h) % 256, 255, 100);
 
     t = drawPixelTextScrolling(&text[0], t, 0);
 
     leds_show();
 
-    delay(1);
+    delay(50);
     h += 2;
 }
